db4041ee440c5bd18c4c2b93f9fee1814624ded9
Ngô Xuân Huy - 60135752 <itnxhuy1806@gmail.com>
2022-03-21 12:23:16 +0700

    Update 11 - Produce a single value from an Array with reduce.js

--- 11 - Produce a single value from an Array with reduce.js
+++ 11 - Produce a single value from an Array with reduce.js
@@ -1,8 +1,9 @@
 let a = [1, 2, 3, 4, 5];
 let sum = a.reduce((prev, item) => { //prev = a[0]
-     return item;
+     return prev+ item;
 })
 console.log(sum);
+
 let users = [
      { 'id': '01', 'name': 'A' },
      { 'id': '02', 'name': 'B' }
